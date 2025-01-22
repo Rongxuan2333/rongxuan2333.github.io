b73e3ece122e00623fb6d452729bf7b986453c5f
ElofHew <2352235176@qq.com>
2025-01-22 11:09:08 +0800

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,14 @@
+// Time
+
+function updateTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('time').innerText = `当前时间：${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
